565994effff33539241deb6f5f402ae1925175c9
Daniel Maurice Davis <daniel.maurice.davis@gmail.com>
2019-04-25 00:04:17 +0200

    updated documentation

--- lib/PackageManager/Virtual.pm
+++ lib/PackageManager/Virtual.pm
@@ -5,43 +5,77 @@ use warnings;
 
 =head1 SYNOPSIS
 
-    package My::PackageManager;
+    package Example::Virtual;
     use Moose;
 
+    has 'example_apps' => ( is => 'rw' );
+
     sub query {
-        return (
-            {
-                name    => "app1",
-                version => "1.0",
-            },
+        my $self    = shift;
+        my %args    = @_;
+        my $verbose = $args{verbose};
+        my $pattern = $args{pattern};
+
+        if ( defined $verbose && $verbose == 1 ) {
+            print 'hi';
+        }
+        if ( defined $pattern ) {
+            my $re = qr/$pattern/;
+            return grep { $_->{name} =~ $re } @{ $self->example_apps };
+        }
+        return @{ $self->example_apps };
+    }
+
+    sub install { }
+
+    with 'PackageManager::Virtual';
+
+Considering the package defined above, an example instantiation could be:
+
+    my $obj = Example::Virtual->new(
+        example_apps => [
             {
                 name    => "devTool",
                 version => "3.0.1-3",
             },
             {
+                name    => "app1",
+                version => "1.0",
+            },
+            {
+                name    => "app2",
+                version => "2.1",
+            },
+            {
                 name    => "cool-thing",
                 version => "alpha",
             },
-        );
-    }
-    
-    sub install { }
+        ]
+    );
 
-    with 'PackageManager::Virtual';
-    1;
+As an example, the valid method invocation:
+
+    $obj->query(pattern => 'app');
+
+Would return the value:
+
+    [
+        {
+            name    => "app1",
+            version => "1.0",
+        },
+        {
+            name    => "app2",
+            version => "2.1",
+        },
+    ];
 
 =head1 DESCRIPTION
 
-The interface provides a minimumalist set if functionality 
-required to manage package installations.
+An interface that provides a minimumal functionality required to manage 
+software package installations.
 
 =head2 SUBROUTINES
-
-All functions should be invoked using the infix dereference operator "->"; 
-the first parameter will always be the object instance. A parameter 'verbose'
-is always optional and has a value 0 or 1 (default=0). Commands run with
-verbose=1 are expected to output additional information to STDOUT. These
-parameters are not included in the definitions below.
 
 All functions use named parameters. For example, the function defintion:
     
@@ -51,34 +85,55 @@ Would be invoked like so:
     
     my %result = $obj->func( arg1 => "hello", arg2 => 5);
 
+A parameter named 'verbose' is always optional and has a value 0 or 1
+(default=0). Commands run with verbose=1 are expected to output additional
+information to STDOUT. It is implied in the definitions below.
+
 =head3 QUERY
 
 Returns a filtered list of installed packages.
 
     query( pattern:string ): Array
 
-Where 'pattern' is any valid Perl regular expression. Only packages whose names
-match this parameter will be returned. Every index of the returned array is a
-hash with the following keys-values:
-
-Name of the package.
-
-    name => string
-
-Version of the package.
-
-    version => string
+Where 'pattern' is any valid Perl regular expression. The return value is an
+Array of package_info whose names match 'pattern'.
 
 =head3 INSTALL
 
 Installs a specified package.
 
-    install( package_name:string version:string ): Integer
+    install( package:package_info ): error_code
 
-Where package_name is the name of the package to be installed. The parameter
-'version' specifies the version of the package to install; it is optional. When
-omitted the latest version will be installed. The subroutine will return the
-number zero on success. Otherwise, the return value indicates an error code.
+Where package defines the package to be installed. The 'version' key of the
+package may be omitted. In this case, the latest version will be installed.
+
+=head3 REMOVE
+
+Uninstalls a specified package.
+
+    remove( package_name:string ): error_code
+
+Where package_name is the name of the package to be uninstalled.
+
+=head2 DATA
+
+=head3 PACKAGE_INFO
+
+package_info := A hash value that defines a package. It has the following
+structure:
+
+    (
+        name    => string,
+        version => string
+    )
+
+Where 'name' is the name of the package and 'version' is a specific version
+of the package.
+
+=head3 ERROR_CODE
+
+Is an integer number value. The value zero implies no error. Otherwise, the
+return value indicates an error code.
 
 =cut
 
